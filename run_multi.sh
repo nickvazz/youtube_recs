6f554805bc44b36dc38d355735b00ee64ee964eb
nickvazz <nickvazquezz@gmail.com>
2020-12-24 17:00:29 -0800

    remove wrong minecraft run

--- run_multi.sh
+++ run_multi.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-for run in $(seq 10) do
-
+for run in $(seq 10) 
+do
     python cli.py python 10 https://www.youtube.com/watch?v=rk-f3B-eMkI
     python cli.py fortnite 10 https://www.youtube.com/watch\?v\=RXABo9hm8B8
     python cli.py legal 10 https://www.youtube.com/watch\?v\=a1I7QBCHqng
@@ -11,7 +11,6 @@ for run in $(seq 10) do
     python cli.py reno-911 10 https://www.youtube.com/watch\?v\=LAnwhV0iGa4
     python cli.py iasip 10 https://www.youtube.com/watch\?v\=oLSBbdGlZW4
     python cli.py curb 10 https://www.youtube.com/watch\?v\=mXsF8rv2csE
-    python cli.py minecraft 10 https://www.youtube.com/watch\?v\=mXsF8rv2csE
     python cli.py jordan-peterson 10 https://www.youtube.com/watch?v=aMcjxSThD54
     python cli.py ben-shapiro 10 https://www.youtube.com/watch?v=rE0Z2k6BJI0
     python cli.py sam-harris 10 https://www.youtube.com/watch?v=UO700RmpzCI
@@ -21,5 +20,4 @@ for run in $(seq 10) do
     python cli.py tucker-carlson 10 https://www.youtube.com/watch?v=jJDGjZ5nefE
     python cli.py oann 10 https://www.youtube.com/watch?v=Qs1CZjceOBc
     python cli.py newsmax 10 https://www.youtube.com/watch?v=pGrvhC_cz1c
-
 done
